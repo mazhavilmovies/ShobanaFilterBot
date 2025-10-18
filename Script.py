71e738eb2c3d10a238a80b431fc7223c647a1f21
mazhavilmovies <m2185351@gmail.com>
2025-10-18 21:08:23 +0530

    Update Script.py

--- Script.py
+++ Script.py
@@ -1,11 +1,11 @@
 class script(object):
-    START_TXT = """ 𝖧𝖾𝗒 {} , 𝖨 𝖠𝗆 <a href=https://t.me/Mazhavil_Waseem_bot/> 𝑾𝑨𝑺𝑬𝑬𝑴 </a></b> , 𝖧𝖺𝗉𝗉𝗒 🖤 𝖳𝗈 𝖧𝖺𝗏𝖾 𝖸𝗈𝗎
+    START_TXT = """ 𝗛𝗲𝘆 {} ,𝗜 𝗔𝗺 <a href=https://t.me/Mazhavil_Waseem_bot/> 𝗪𝗮𝘀𝗲𝗲𝗺 </a></b> , 𝗛𝗮𝗽𝗽𝘆 🖤 𝗧𝗼 𝗛𝗮𝘃𝗲 𝗬𝗼𝘂 
 
-𝖨𝖺𝗆 𝖯𝗈𝗐𝖾𝗋𝖥𝗎𝗅𝗅 𝖠𝗎𝗍𝗈 𝖥𝗂𝗅𝗍𝖾𝗋 + 𝖬𝗈𝗏𝗂𝖾 𝖲𝖾𝖺𝗋𝖼𝗁 + 𝖬𝖺𝗇𝗎𝖺𝗅 𝖥𝗂𝗅𝗍𝖾𝗋 𝖡𝗈𝗍 ⚙️
+𝗜𝗮𝗺 𝗣𝗼𝘄𝗲𝗿𝗳𝘂𝗹𝗹 𝗔𝘂𝘁𝗼 𝗙𝗶𝗹𝘁𝗲𝗿 + 𝗠𝗼𝘃𝗶𝗲 𝗦𝗲𝗮𝗿𝗰𝗵 + 𝗠𝗮𝗻𝘂𝗮𝗹 𝗙𝗶𝗹𝘁𝗲𝗿 𝗕𝗼𝘁 ⚙
 
-Hᴇʀᴇ Yᴏᴜ Cᴀɴ Rᴇǫᴜᴇsᴛ Mᴏᴠɪᴇ's, Jᴜsᴛ Sᴇɴᴛ <a href=https://t.me/Mazhavil_Waseem_bot/> Mᴏᴠɪᴇ Nᴀᴍᴇ </a></b> Wɪᴛʜ Pʀᴏᴘᴇʀ <a href=https://www.google.com/> Gᴏᴏɢʟᴇ </a></b> Sᴘᴇʟʟɪɴɢ..!!
+𝗛𝗲𝗿𝗲 𝗬𝗼𝘂 𝗖𝗮𝗻 𝗥𝗲𝗾𝘂𝗲𝘀𝘁 𝗠𝗼𝘃𝗶𝗲'𝘀, 𝗝𝘂𝘀𝘁 𝗦𝗲𝗻𝘁 <a href=https://t.me/Mazhavil_Waseem_bot/> 𝗠𝗼𝘃𝗶𝗲 𝗡𝗮𝗺𝗲 </a></b> 𝗪𝗶𝘁𝗵 𝗣𝗿𝗼𝗽𝗲𝗿 <a href=https://www.google.com/> 𝗚𝗼𝗼𝗴𝗹𝗲 </a></b> 𝗦𝗽𝗲𝗹𝗹𝗶𝗻𝗴..!!
 
-Cᴏɴᴛᴀᴄᴛ Bᴏᴛ Dᴇᴠᴇʟᴏᴘᴇʀ (Oʀ) Rᴇᴘᴏʀᴛ Bᴜɢꜱ..!! 👉 @MazhavilAdminBot</b>"""
+𝗖ᴏɴᴛᴀᴄᴛ 𝗕ᴏᴛ 𝗗ᴇᴠᴇʟᴏᴘᴇʀ (𝗢ʀ) 𝗥ᴇᴘᴏʀᴛ 𝗕ᴜɢꜱ..!! 👉 @MAZHAVILADMINBOT</b>"""
     HELP_TXT = """
      Hey  {} 
 𝙷𝙴𝚁𝙴 𝙸𝚂 𝚃𝙷𝙴 𝙷𝙴𝙻𝙿 𝙵𝙾𝚁 𝙼𝚈 𝙲𝙾𝙼𝙼𝙰𝙽𝙳𝚂....
